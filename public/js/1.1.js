e0190e27af54d7e645396de94ee08832d32f089a
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2018-07-25 01:27:19 +0300

    removed files

--- public/js/1.1.js
+++ public/js/1.1.js
@@ -1,13 +1,16 @@
-webpackJsonp([1],{
-
-/***/ 2:
+webpackJsonp([1],[
+/* 0 */,
+/* 1 */
 /***/ (function(module, exports) {
 
 	'use strict';
 
-	alert("Dynamic works!");
+	var elems = document.querySelectorAll('.parallax');
+	var instances = M.Parallax.init(elems);
+
+	$(".categories-trigger").dropdown();
+	$(".lang-trigger").dropdown();
 
 /***/ })
-
-});
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMS4xLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vL2Zyb250ZW5kL2pzL2hlYWRlci9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIndXNlIHN0cmljdCc7XG5cbmFsZXJ0KFwiRHluYW1pYyB3b3JrcyFcIik7XG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIGZyb250ZW5kL2pzL2hlYWRlci9pbmRleC5qcyJdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtBQUNBO0FBQ0E7Ozs7Iiwic291cmNlUm9vdCI6IiJ9
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMS4xLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vL2Zyb250ZW5kL2pzL2hlYWRlci9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIndXNlIHN0cmljdCc7XHJcblxyXG52YXIgZWxlbXMgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yQWxsKCcucGFyYWxsYXgnKTtcclxudmFyIGluc3RhbmNlcyA9IE0uUGFyYWxsYXguaW5pdChlbGVtcyk7XHJcblxyXG4kKFwiLmNhdGVnb3JpZXMtdHJpZ2dlclwiKS5kcm9wZG93bigpO1xyXG4kKFwiLmxhbmctdHJpZ2dlclwiKS5kcm9wZG93bigpO1xuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyBmcm9udGVuZC9qcy9oZWFkZXIvaW5kZXguanMiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7OzsiLCJzb3VyY2VSb290IjoiIn0=
